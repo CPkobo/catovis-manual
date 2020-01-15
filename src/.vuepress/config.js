f0573f2ae343df1c0cb674a6c9b7be089d9f3bb2
CPkobo <31347658+CPkobo@users.noreply.github.com>
2020-01-15 19:17:15 +0900

    base fixed

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,11 +1,12 @@
 module.exports = {
+    base: '/catovis-docs/',
     themeConfig: {
         sidebar: 'auto',
         nav: [
             { text: 'ABOUT', link: '/' },
             { text: 'Get Started', items: [
-                { text: 'Install', link: '/install/' },
-                { text: 'Basic Flow (Word)', link: '/basicflow/' },
+                { text: 'Install', link: '/getstarted/install' },
+                { text: 'Basic Flow (Word)', link: '/getstarted/withword' },
                 ]
             },
             { text: 'Filter', link: '/filter/' },
