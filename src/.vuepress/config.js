067ce9e7cfc03a4227d429fd16bcf51ef4a8a0ee
CPkobo <31347658+CPkobo@users.noreply.github.com>
2020-01-15 19:01:37 +0900

    initial commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    themeConfig: {
+        sidebar: 'auto',
+        nav: [
+            { text: 'ABOUT', link: '/' },
+            { text: 'Get Started', items: [
+                { text: 'Install', link: '/install/' },
+                { text: 'Basic Flow (Word)', link: '/basicflow/' },
+                ]
+            },
+            { text: 'Filter', link: '/filter/' },
+        ]
+    }
+}
