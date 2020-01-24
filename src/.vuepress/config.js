4b31e6d280c2f17485307e6120d4ffba3aec418a
QuanKaoYang <ishida.goldenbridge2002@gmail.com>
2020-01-24 14:22:19 +0900

    update

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,17 +4,31 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', type: 'image/png', href: '/catovis.ico' }],
     ],
+	plugins: [
+    	[
+			'@vuepress/google-analytics',
+    	  	{ 'ga': 'UA-155988647-2' }
+    	],
+	],
     themeConfig: {
         sidebar: 'auto',
         nav: [
-            { text: 'ABOUT', link: '/' },
-            { text: 'Get Started', items: [
-                { text: 'Install', link: '/getstarted/install' },
-                { text: 'Basic Flow (Word)', link: '/getstarted/withword' },
+            { text: 'トップ', link: '/' },
+            { text: '始めよう', items: [
+                { text: 'インストール', link: '/getstarted/01_install' },
+                { text: '基本の使い方 Word編', link: '/getstarted/02_withword' },
+                { text: 'ファイルの読み込み', link: '/getstarted/03_open_import' },
+                { text: 'ファイルのエクスポート', link: '/getstarted/04_export' },
                 ]
             },
-            { text: 'Filter', link: '/filter/' },
-            { text: 'LICENSE', link: '/LICENSE/' },
+			{ text: '便利な機能', items: [
+				{ text: 'フィルター', link: '/features/01_filter' },
+				{ text: 'スタンドアロン', link: '/features/02_standalone' },
+				]
+			},
+            { text: 'FAQ', link: '/faq/' },
+            { text: 'ライセンス', link: '/LICENSE/' },
+			{ text: 'プライバシーポリシー', link: '/PRIVACY/' },
         ]
     }
 }
